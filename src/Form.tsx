2727477442de5d30b16d1d45240db3e5b85655db
jurekg1 <jurek@poczta.pl>
2025-04-11 08:58:31 +0200

    Put form in a block and added all parameters

--- src/Form.tsx
+++ src/Form.tsx
@@ -5,6 +5,7 @@ import "./Form.css"
 interface FormData{
     name: string;
     reason: string;
+    date: string;
     believability: string ;
     creativity: string;
     extra:string;
@@ -15,6 +16,7 @@ const Form = () => {
     const [formData, setFormData] = useState<FormData>({
         name:"",
         reason:"",
+        date:"",
         believability:"",
         creativity:"",
         extra:"",
@@ -30,4 +32,93 @@ const Form = () => {
         const {name, value} = event.target
         setFormData(prevState => ({...prevState, [name]: value}))
     }
+
+    const today = new Date().toISOString().split('T')[0];
+
+    return(
+        <div>
+            <h2>Generator wymówek</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nazwa
+                    <input
+                        type="text" name="name"
+                        value={formData.name}
+                        onChange={handleChange}/>
+                </label>
+
+                <label>
+                    Powód
+                    <select
+                        name="reason"
+                        value={formData.reason}
+                        onChange={handleChange}>
+
+                        <option disabled>Wybierz z listy</option>
+                        <option>Spóźnienie</option>
+                        <option>Brak pracy domowej</option>
+                        <option>Nieobecność</option>
+                        <option>Brak pracy na lekcji</option>
+                    </select>
+                </label>
+
+                <label>
+                    Poziom wiarygodności
+                    <input
+                        type="range" name="believability"
+                        value={formData.believability}
+                        min="1" max="10" step="1"
+                        onChange={handleChange}/>
+                </label>
+
+
+                <label>
+                    Data zdarzenia
+                    <input
+                        type="date" name="date"
+                        value={formData.date}
+                        min="2025-01-01"
+                        max={today}
+                        onChange={handleChange}/>
+                </label>
+
+                <label>
+                    Poziom kreatywności
+                    <select
+                        name="creativity"
+                        value={formData.creativity}
+                        onChange={handleChange}>
+
+                        <option disabled>Wybierz z listy</option>
+                        <option>Tragedia</option>
+                        <option>Kiepsko</option>
+                        <option>Średnio</option>
+                        <option>Nawet nawet</option>
+                        <option>Całkiem oryginalne</option>
+                    </select>
+                </label>
+
+                <label>
+                    Dodatkowe szczegóły
+                    <textarea
+                        name="extra"
+                        value={formData.extra}
+                        onChange={handleChange}/>
+                </label>
+
+                <label>
+                    Pilna
+                    <input
+                        type="checkbox" name="urgency"
+                        value={formData.urgency}
+                        onChange={handleChange}>
+                    </input>
+                </label>
+            </form>
+        </div>
+    )
+
+
 }
+
+export default Form
