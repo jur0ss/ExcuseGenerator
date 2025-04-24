e9deb103e487a13163c5a5fdd8ccfb0ee952e942
jurekg <jurek.gryz2007@gmail.com>
2025-04-24 22:16:08 +0200

    Created child and its interface

---
+++ src/Child.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from "react";
+
+interface Props {
+    data: {
+        name: string;
+        reason: string;
+        date: string;
+        believability: number;
+        creativity: string;
+        extra: string;
+        urgency: boolean;
+    };
+}
