5eef8f68248040763f57eaeac877b707d9dd60d4
jurekg <jurek.gryz2007@gmail.com>
2025-04-24 22:27:49 +0200

    Created a div which displays excuse as a list and exported Child

--- src/Child.tsx
+++ src/Child.tsx
@@ -2,12 +2,32 @@
 
 interface Props {
     data: {
-        name: string;
-        reason: string;
-        date: string;
-        believability: number;
-        creativity: string;
-        extra: string;
-        urgency: boolean;
-    };
+        name: string,
+        reason: string,
+        date: string,
+        believability: number,
+        creativity: string,
+        extra: string,
+        urgency: boolean
+    }
 }
+
+
+const Child: React.FC<Props> = ({ data }) => {
+    return (
+        <div>
+            <h3>Twoje wymówki:</h3>
+            <ul>
+                <li>Nazwa: {data.name}</li>
+                <li>Powód: {data.reason}</li>
+                <li>Data zdarzenia: {data.date}</li>
+                <li>Wiarygodność: {data.believability}/10</li>
+                <li>Poziom kreatywności: {data.creativity}</li>
+                <li>Dodatkowe szczegóły: {data.extra}</li>
+                <li>Pilna: {data.urgency ? "Tak" : "Nie"}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Child
